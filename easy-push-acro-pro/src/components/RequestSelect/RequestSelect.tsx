f7ffbacdfbf8a145cc00de74047b09c3c5286aac
yuzhanfeng <yuzhanfeng@foxmail.com>
2022-09-27 09:36:46 +0800

    arco pro

--- easy-push-acro-pro/src/components/RequestSelect/RequestSelect.tsx
+++ easy-push-acro-pro/src/components/RequestSelect/RequestSelect.tsx
@@ -3,53 +3,53 @@ import { AxiosPromise } from 'axios';
 import React, { useEffect, useState } from 'react';
 
 export interface props{
-  value?:string;
-  placeholder?:string;
-  onChange?:(value)=>void;
-  lableFiled:string;
-  valueFiled:string;
-  request?:()=>AxiosPromise<any>;
+    value?:string;
+    placeholder?:string;
+    onChange?:(value)=>void;
+    lableFiled:string;
+    valueFiled:string;
+    request?:()=>AxiosPromise<any>;
 }
 
 function RequestSelect(props?:props) {
 
-  const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-  const [options, setOptions] = useState([]);
+    const [options, setOptions] = useState([]);
 
-  useEffect(() => {
-    setLoading(true);
-    props.request().then((res) => {
-      const { success, data } = res.data;
-      if (success) {
-        const newOptions = [];
-        data.map((item) => {
-          newOptions.push({
-              value: item[props.valueFiled],
-              label: item[props.lableFiled],
-              key: item[props.lableFiled],
-            });
+    useEffect(() => {
+        setLoading(true);
+        props.request().then((res) => {
+            const { success, data } = res.data;
+            if (success) {
+                const newOptions = [];
+                data.map((item) => {
+                    newOptions.push({
+                        value: item[props.valueFiled],
+                        label: item[props.lableFiled],
+                        key: item[props.lableFiled],
+                    });
+                });
+                setOptions(newOptions);
+            }
+            setLoading(false);
         });
-        setOptions(newOptions);
-      }
-      setLoading(false);
-    });
-  }, []);
+    }, [props.lableFiled]);
 
-  return (
-    <Select
-      value={props.value}
-      placeholder={props.placeholder}
-      loading={loading}
-      showSearch
-      options={options}
-      onChange={props.onChange}
-      filterOption={(inputValue, option) =>
-        option.props.children.toLowerCase().indexOf(inputValue.toLowerCase()) >=
-        0
-      }
-    />
-  );
+    return (
+        <Select
+            value={props.value}
+            placeholder={props.placeholder}
+            loading={loading}
+            showSearch
+            options={options}
+            onChange={props.onChange}
+            filterOption={(inputValue, option) =>
+                option.props.children.toLowerCase().indexOf(inputValue.toLowerCase()) >=
+                0
+            }
+        />
+    );
 }
 
 export default RequestSelect;
