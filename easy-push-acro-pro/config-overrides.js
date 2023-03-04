421b1428e8451bd5d4e3c6decec9d4fd5fa30419
于占峰 <yuzhanfeng@yunlizhihui.com>
2023-03-04 13:36:50 +0800

    去掉分析插件

--- easy-push-acro-pro/config-overrides.js
+++ easy-push-acro-pro/config-overrides.js
@@ -37,9 +37,9 @@ module.exports = {
         languages: ['apex', 'azcli', 'bat', 'clojure', 'coffee', 'cpp', 'csharp', 'csp', 'css', 'dockerfile', 'fsharp', 'go', 'handlebars', 'html', 'ini', 'java', 'javascript', 'json', 'less', 'lua', 'markdown', 'msdax', 'mysql', 'objective', 'perl', 'pgsql', 'php', 'postiats', 'powerquery', 'powershell', 'pug', 'python', 'r', 'razor', 'redis', 'redshift', 'ruby', 'rust', 'sb', 'scheme', 'scss', 'shell', 'solidity', 'sql', 'st', 'swift', 'typescript', 'vb', 'xml', 'yaml']
       })
     ),
-    addWebpackPlugin(
-      new BundleAnalyzerPlugin()
-    ),
+    // addWebpackPlugin(
+    //   new BundleAnalyzerPlugin()
+    // ),
     addWebpackAlias({
       '@': path.resolve(__dirname, 'src'),
       "@components": path.resolve(__dirname, "src/components"),
