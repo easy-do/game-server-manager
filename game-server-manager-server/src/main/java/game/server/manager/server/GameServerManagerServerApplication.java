36ef584b99a6dfbf7205df31084ef94d65b98069
于占峰 <yuzhanfeng@yunlizhihui.com>
2023-03-23 19:44:41 +0800

    test

--- game-server-manager-server/src/main/java/game/server/manager/server/GameServerManagerServerApplication.java
+++ game-server-manager-server/src/main/java/game/server/manager/server/GameServerManagerServerApplication.java
@@ -1,6 +1,7 @@
 package game.server.manager.server;
 
 import com.alicp.jetcache.anno.config.EnableMethodCache;
+import game.server.manager.server.netty.CoordinationNettyServer;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,6 +22,11 @@ public class GameServerManagerServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GameServerManagerServerApplication.class, args);
+//        try {
+//            new CoordinationNettyServer(8804).start();
+//        } catch (Exception e) {
+//            System.out.println("NettyServerError:" + e.getMessage());
+//        }
     }
 
 }
