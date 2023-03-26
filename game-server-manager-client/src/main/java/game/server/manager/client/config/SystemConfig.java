b0ef7cbb8c2375c4ba0b7d2b7d0f63107a5a29bc
gebilaoyu <gebilaoyu@foxmail.com>
2023-03-26 14:53:41 +0800

    实例化方式调整

--- game-server-manager-client/src/main/java/game/server/manager/client/config/SystemConfig.java
+++ game-server-manager-client/src/main/java/game/server/manager/client/config/SystemConfig.java
@@ -6,12 +6,16 @@ import com.github.dockerjava.core.DockerClientBuilder;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 import com.github.dockerjava.transport.DockerHttpClient;
+import game.server.manager.client.websocket.ClientWebsocketEndpoint;
+import game.server.manager.client.websocket.WebSocketClientHandlerService;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.Duration;
 
 
@@ -25,9 +29,6 @@ import java.time.Duration;
 @Data
 @Configuration(proxyBeanMethods = false)
 public class SystemConfig {
-
-    @Autowired
-    private SystemUtils systemUtils;
 
     @Bean
     public DockerClient dockerClient(){
@@ -44,5 +45,15 @@ public class SystemConfig {
         return DockerClientBuilder.getInstance(dockerClientConfig).withDockerHttpClient(httpClient)
                 .build();
     }
+
+    @Bean
+    public ClientWebsocketEndpoint clientWebsocketEndpoint(SystemUtils systemUtils, WebSocketClientHandlerService webSocketClientHandlerService) throws URISyntaxException {
+        URI serverSocketUrI = new URI(systemUtils.getServerSocketUrl());
+        ClientWebsocketEndpoint clientWebsocketEndpoint = new ClientWebsocketEndpoint(serverSocketUrI);
+        clientWebsocketEndpoint.setSystemUtils(systemUtils);
+        clientWebsocketEndpoint.setHandlerService(webSocketClientHandlerService);
+        return clientWebsocketEndpoint;
+    }
+
 
 }
