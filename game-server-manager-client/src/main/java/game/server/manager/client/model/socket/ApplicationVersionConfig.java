83bfc5fa4af4186ad3f64681a8be5aeffc299514
gebilaoyu <gebilaoyu@foxmail.com>
2023-04-08 18:36:11 +0800

    应用安装增强功能初步完成

--- game-server-manager-client/src/main/java/game/server/manager/client/model/socket/ApplicationVersionConfig.java
+++ game-server-manager-client/src/main/java/game/server/manager/client/model/socket/ApplicationVersionConfig.java
@@ -2,6 +2,7 @@ package game.server.manager.client.model.socket;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import game.server.manager.client.model.BindDto;
+import game.server.manager.client.model.CreateNetworkDto;
 import game.server.manager.client.model.PortBindDto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,7 @@ import java.util.Map;
 /**
  * @author laoyu
  * @version 1.0
- * @description TODO
+ * @description 应用版本信息
  * @date 2023/4/1
  */
 
@@ -20,31 +21,58 @@ import java.util.Map;
 @Data
 public class ApplicationVersionConfig {
 
+    @JsonProperty("createNetworks")
+    private Boolean createNetworks;
+    @JsonProperty("networks")
+    private List<CreateNetworkDto> networks;
+    @JsonProperty("subApps")
+    private List<SubApps> subApps;
 
-    @JsonProperty("key")
-    private Integer key;
-    @JsonProperty("name")
-    private String name;
-    @JsonProperty("version")
-    private String version;
-    @JsonProperty("image")
-    private String image;
-    @JsonProperty("cmd")
-    private String cmd;
-    @JsonProperty("attachStdin")
-    private Boolean attachStdin;
-    @JsonProperty("stdinOpen")
-    private Boolean stdinOpen;
-    @JsonProperty("privileged")
-    private Boolean privileged;
-    @JsonProperty("tty")
-    private Boolean tty;
-    @JsonProperty("networkMode")
-    private String networkMode;
-    @JsonProperty("envs")
-    private List<Map<String,String>> envs;
-    @JsonProperty("portBinds")
-    private List<PortBindDto> portBinds;
-    @JsonProperty("binds")
-    private List<BindDto> binds;
+    @NoArgsConstructor
+    @Data
+    public static class SubApps{
+        @JsonProperty("key")
+        private Integer key;
+        @JsonProperty("name")
+        private String name;
+        @JsonProperty("version")
+        private String version;
+        @JsonProperty("image")
+        private String image;
+        @JsonProperty("cmd")
+        private String cmd;
+        @JsonProperty("labels")
+        private String labels;
+        @JsonProperty("links")
+        private String links;
+        @JsonProperty("attachStdin")
+        private Boolean attachStdin;
+        @JsonProperty("stdinOpen")
+        private Boolean stdinOpen;
+        @JsonProperty("privileged")
+        private Boolean privileged;
+        @JsonProperty("tty")
+        private Boolean tty;
+        @JsonProperty("networkMode")
+        private String networkMode;
+        @JsonProperty("envs")
+        private List<Map<String,String>> envs;
+        @JsonProperty("portBinds")
+        private List<PortBindDto> portBinds;
+        @JsonProperty("binds")
+        private List<BindDto> binds;
+        @JsonProperty("publishAllPorts")
+        private Boolean publishAllPorts;
+        @JsonProperty("nanoCPUs")
+        private Long nanoCPUs;
+        @JsonProperty("memory")
+        private Long memory;
+        @JsonProperty("shmSize")
+        private Long shmSize;
+        @JsonProperty("memorySwap")
+        private Long memorySwap;
+        @JsonProperty("restartPolicy")
+        private String restartPolicy;
+    }
+
 }
