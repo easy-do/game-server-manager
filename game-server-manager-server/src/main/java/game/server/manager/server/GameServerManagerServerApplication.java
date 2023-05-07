15e8480911ab7210498cc7c7f9c092623b8fb46c
gebilaoyu <gebilaoyu@foxmail.com>
2023-05-07 16:09:09 +0800

    @EnableScheduling

--- game-server-manager-server/src/main/java/game/server/manager/server/GameServerManagerServerApplication.java
+++ game-server-manager-server/src/main/java/game/server/manager/server/GameServerManagerServerApplication.java
@@ -8,10 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author yuzhanfeng
  */
+@EnableScheduling
 @EnableAsync
 @EnableMethodCache(basePackages = "game.server.manager")
 @EnableFeignClients(basePackages = {"game.server.manager"})
