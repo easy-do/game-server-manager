6ad37f6a23cf68e4d240d49141697c0fe24b8c4c
于占峰 <yuzhanfeng@yunlizhihui.com>
2023-05-14 03:31:04 +0800

    DockerCompose

---
+++ game-server-manager-server/src/main/java/game/server/manager/server/Test.java
@@ -0,0 +1,31 @@
+package game.server.manager.server;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.json.JSONConfig;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import game.server.manager.docker.compose.DockerCompose;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+
+
+/**
+ * @author laoyu
+ * @version 1.0
+ * @description TODO
+ * @date 2023/5/13
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        Yaml yaml = new Yaml();
+        File file = FileUtil.file("E:/docker-compose.yaml");
+        JSONObject ymlJson = yaml.loadAs(FileUtil.getInputStream(file),JSONObject.class);
+        DockerCompose dockerCompose = ymlJson.toBean(DockerCompose.class);
+        System.err.println(JSONUtil.toJsonStr(dockerCompose, JSONConfig.create().setIgnoreNullValue(false)));
+
+        System.err.println(yaml.dump(JSONUtil.parseObj(dockerCompose,JSONConfig.create().setIgnoreNullValue(true))));
+    }
+}
+
