39f78152ba19cf64f55a82416ebcdc415733b52c
于占峰 <yuzhanfeng@yunlizhihui.com>
2023-06-24 14:23:09 +0800

    15%

--- commons/easy-do-plus-web/src/main/java/plus/easydo/web/config/FastJsonConfiguration.java
+++ commons/easy-do-plus-web/src/main/java/plus/easydo/web/config/FastJsonConfiguration.java
@@ -1,29 +1,29 @@
-package plus.easydo.web.config;
-
-
-import com.alibaba.fastjson2.support.config.FastJsonConfig;
-import com.alibaba.fastjson2.support.spring.http.converter.FastJsonHttpMessageConverter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
-
-import java.util.Arrays;
-
-
-/**
- * @author laoyu
- * @version 1.0
- * @date 2022/9/4
- */
-@Configuration(proxyBeanMethods = false)
-public class FastJsonConfiguration {
-
-    @Bean
-    public FastJsonHttpMessageConverter fastJsonHttpMessageConverter() {
-        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
-        converter.setFastJsonConfig(new FastJsonConfig());
-        converter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON, new MediaType("application", "*+json")));
-        return converter;
-    }
-
-}
+//package plus.easydo.web.config;
+//
+//
+//import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+//import com.alibaba.fastjson.support.config.FastJsonConfig;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.MediaType;
+//
+//import java.util.Arrays;
+//
+//
+///**
+// * @author laoyu
+// * @version 1.0
+// * @date 2022/9/4
+// */
+//@Configuration(proxyBeanMethods = false)
+//public class FastJsonConfiguration {
+//
+//    @Bean
+//    public FastJsonHttpMessageConverter fastJsonHttpMessageConverter() {
+//        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+//        converter.setFastJsonConfig(new FastJsonConfig());
+//        converter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON, new MediaType("application", "*+json")));
+//        return converter;
+//    }
+//
+//}
--- commons/easy-do-plus-web/src/main/java/plus/easydo/web/config/WebMvcConfig.java
+++ commons/easy-do-plus-web/src/main/java/plus/easydo/web/config/WebMvcConfig.java
@@ -1,39 +1,39 @@
-package plus.easydo.web.config;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
-
-/**
- * @author laoyu
- * @version 1.0
- * @description WebMvc配置
- * @date 2023/3/5
- */
-
-@Configuration
-@Slf4j
-public class WebMvcConfig implements WebMvcConfigurer {
-
-    /**
-     * 扩展mvc框架的消息转换器
-     * @param converters
-     */
-    @Override
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-        log.info("扩展消息转换器...");
-        //创建消息转换器对象
-        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
-        //设置对象转换器，底层使用Jackson将Java对象转为json
-        messageConverter.setObjectMapper(new JacksonObjectMapper());
-        //将消息转换器对象追加到mvc框架的转换器集合中
-        converters.add(0,messageConverter);
-    }
-
-
-
-}
+//package plus.easydo.web.config;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.converter.HttpMessageConverter;
+//import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import java.util.List;
+//
+///**
+// * @author laoyu
+// * @version 1.0
+// * @description WebMvc配置
+// * @date 2023/3/5
+// */
+//
+//@Configuration
+//@Slf4j
+//public class WebMvcConfig implements WebMvcConfigurer {
+//
+//    /**
+//     * 扩展mvc框架的消息转换器
+//     * @param converters
+//     */
+//    @Override
+//    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        log.info("扩展消息转换器...");
+//        //创建消息转换器对象
+//        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+//        //设置对象转换器，底层使用Jackson将Java对象转为json
+//        messageConverter.setObjectMapper(new JacksonObjectMapper());
+//        //将消息转换器对象追加到mvc框架的转换器集合中
+//        converters.add(0,messageConverter);
+//    }
+//
+//
+//
+//}
